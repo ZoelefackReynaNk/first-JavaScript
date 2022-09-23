bf62958a17dbba4c84a71fca1f22e52c0bb0a436
Zoelefack Reyna Nkoumbouk <zoelefackreyna246@gmail.com>
2022-09-23 09:10:54 +0100

    Update Snai in the well.js

--- Snai in the well.js
+++ Snai in the well.js
@@ -1,1 +1,15 @@
 
+function main() {
+    var depth = parseInt(readLine(), 10);
+    //your code goes her
+    if (depth % 5 == 0) {
+        console.log(depth / 5);
+    }
+    else if (depth % 5 >= 3) {
+        console.log(parseInt(depth / 5) + 1)
+    }
+    else {
+        console.log(parseInt(depth / 5));
+    }
+}
+
